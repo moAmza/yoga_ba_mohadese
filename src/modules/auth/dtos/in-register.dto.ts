f57ad44f6a6c936b271705c8d507d6368db18cae
moAmza <mohamad.zangooei@outlook.com>
2023-08-31 12:35:13 +0330

    feat: change register error messages in zod

--- src/modules/auth/dtos/in-register.dto.ts
+++ src/modules/auth/dtos/in-register.dto.ts
@@ -11,32 +11,32 @@ import {
 export class InRegisterDto {
   @ApiProperty({ required: true, default: 'username' })
   @IsString()
-  @MinLength(8)
+  @MinLength(8, { message: 'نام کاربری باید حداقل ۸ کاراکتر داشته باشد' })
   @Transform((param) => param.value.toLowerCase())
   username: string;
 
   @ApiProperty({ required: true, default: 'password' })
   @IsString()
-  @MinLength(8)
+  @MinLength(8, { message: 'کلمه عبور باید حداقل ۸ کاراکتر داشته باشد' })
   password: string;
 
   @ApiProperty({ required: true, default: 'firstname' })
   @IsString()
   firstname: string;
 
-  @ApiProperty({ required: true, default: 'lastnamee' })
+  @ApiProperty({ required: true, default: 'lastname' })
   @IsString()
   lastname: string;
 
   @ApiProperty({ required: false, default: 'email@email.email' })
   @IsOptional()
   @IsString()
-  @IsEmail()
+  @IsEmail({}, { message: 'ایمیل نامعتبر است' })
   @Transform((param) => param.value.toLowerCase())
   email?: string;
 
   @ApiProperty({ required: false, default: '09120000000' })
   @IsString()
-  @IsPhoneNumber('IR')
+  @IsPhoneNumber('IR', { message: 'شماره تلفن نامعتبر است' })
   phone: string;
 }
